8f4f708539d83323e514267cab5f41c0d2756b13
JKD_Antonio <antonio.villalba.us@gmail.com>
2018-10-17 22:28:12 -0400

    Uploading Twitter Sentiment Analysis

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "mDPHe8OfF9jnStZSQes4aAdbY"
+consumer_secret = "FhloNERJcQJWbXDXLXvImXogGV878pA5IRZcOXvn2CDzsZajbn"
+access_token = "2807838573-OObM1Ua76W3yiDJCpaIman7HEKUTAMe6TmBOjfO"
+access_token_secret = "Ygf2YwAZK0ihsoX8azV6McmdeFailc2bP4SwrVgVXFUgi"
